3b0f099c1739422345ec8125987c03a621c3937b
Назар Кривоніс <nazarkrivonis@gmail.com>
2020-04-22 22:02:34 +0300

    added mysql2 connection

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const mysql = require('mysql2');
+
 const app = express();
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'nazarino203',
+    database: 'chemiplast_data'
+});
+
+pool.query('SELECT * FROM orders', (err, result) => { 
+    console.log(result[0]);
+});
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res, next) => {
     res.send('Hello App!');
