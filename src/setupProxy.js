ec221fe36a1813e6a248bbbe292b3d4e3c98cbe9
Назар Кривоніс <nazarkrivonis@gmail.com>
2020-05-13 18:58:42 +0300

    rpoxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,22 +1,22 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-// module.exports = function(app) {
-//     app.use(
-//         '/',
-//         createProxyMiddleware({
-//           target: 'https://damp-plains-96902.herokuapp.com/',
-//           changeOrigin: true,
-//         })
-//       );
-// }
+module.exports = function(app) {
+    app.use(
+        '/',
+        createProxyMiddleware({
+          target: 'https://damp-plains-96902.herokuapp.com/',
+          changeOrigin: true,
+        })
+      );
+}
 
 // Local Dev
-module.exports = function(app) {
-  app.use(
-      '/api',
-      createProxyMiddleware({
-        target: 'https://localhost:5000/',
-        changeOrigin: true,
-      })
-    );
+// module.exports = function(app) {
+//   app.use(
+//       '/api',
+//       createProxyMiddleware({
+//         target: 'https://localhost:5000/',
+//         changeOrigin: true,
+//       })
+//     );
 }
