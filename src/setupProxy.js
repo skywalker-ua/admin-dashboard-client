cefbf6e3e0fd75a704869006c9aa9b1cc9a3a639
Назар Кривоніс <nazarkrivonis@gmail.com>
2020-05-24 17:23:04 +0300

    cors

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
     app.use(
-        '/api',
+        '/',
         createProxyMiddleware({
           target: 'https://damp-plains-96902.herokuapp.com/',
           changeOrigin: true,
